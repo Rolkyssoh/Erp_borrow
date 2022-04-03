23447be896c99c9143314912cbf3936eddd72c37
Rolkyssoh <juventussoh@gmail.com>
2022-04-03 12:38:47 +0000

    My first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {HomeScreen} from './src/screens';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
